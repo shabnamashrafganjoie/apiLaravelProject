a4bda6af935b8cf9c20558970ca948d81381947f
ShabnamAshrafGanjoie <shabnam.ashraf.ganjoie@gmail.com>
2023-07-29 22:14:43 +0330

    first commit

---
+++ app/Models/category.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class category extends Model
+{
+    use HasFactory,SoftDeletes;
+    protected $table='categories';
+    protected $guarded=[];
+
+}
---
+++ database/migrations/2023_07_29_180915_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('brand_id')->constrained('brands')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('primary_image')->nullable();
+            $table->text('description')->nullable();
+            $table->unsignedInteger('price')->default(0);
+            $table->unsignedInteger('quantity')->default(0);
+            $table->unsignedInteger('delivery_amount')->default(0);
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
